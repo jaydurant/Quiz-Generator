770dfa3b9d549859982a1418623b4f85435e350f
durantjay <durantjay1@gmail.com>
2015-12-02 12:50:14 -0800

    Add script Gulp task to the Gulpfile.js to minify and concat js files into the js directory

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,18 +3,38 @@ var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
 var concat = require('gulp-concat');
+var minifycss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var jshint =require("gulp-jshint")
+var del = require('del');
+
 
 gulp.task('styles',function(){
 	gulp.src('sass/**/*.scss')
-		.pipe(sourcemaps.init())
 		.pipe(sass().on('error', sass.logError))
 		.pipe(autoprefixer())
 		.pipe(concat('style.css'))
-		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('./css/'))
+		.pipe(rename({suffix:'.min'}))
+		.pipe(minifycss())
 		.pipe(gulp.dest('./css/'));
 });
+
+gulp.task('scripts',function(){
+	gulp.src('test-js/**/*.js')
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'))
+		.pipe(concat('main.js'))
+		.pipe(gulp.dest('./js/'))
+		.pipe(rename({suffix:'.min'}))
+		.pipe(uglify())
+		.pipe(gulp.dest('./js/'))
+})
+
 
 //Watch task
 gulp.task("default",function(){
 	gulp.watch('sass/**/*.scss',['styles'])
+	gulp.watch('test-js/**/*.js',['scripts'])
 }); 
