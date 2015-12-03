4dd82028f6ff9aaf8d6f11aa6e1095edc35565c3
durantjay <durantjay1@gmail.com>
2015-12-03 01:07:28 -0800

    Add comments to jsonrequest.js detailing the AJAX request

--- test-js/jsonrequest.js
+++ test-js/jsonrequest.js
@@ -1,13 +1,16 @@
 function HttpRequest(sUrl,fpCallback){
-	request = new XMLHttpRequest();
+	//a new asynchronous request is created
+	var request = new XMLHttpRequest();
 	request.open("Get",sUrl,true);
 
 	var tempRequest = request;
+	//the request_readystate function detects whether the request for data was successful by checking for the 200 status
 	function request_readystatechange(){
 		if(tempRequest.readyState === 4 ){
 			if(tempRequest.status === 200){
 				fpCallback(tempRequest.responseText);
 			}
+			//if the requst was not successful the error status code can be seen in the 
 			else{
 				console.log("An error occured trying to connect to the server:" + tempRequest.status);
 			}
