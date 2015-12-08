eae7d030c850cca741847c7c61689fc4faea7716
durantjay <durantjay1@gmail.com>
2015-12-08 11:18:51 -0800

    Made variable assignment  fixes to the http request js document

--- test-js/jsonrequest.js
+++ test-js/jsonrequest.js
@@ -3,12 +3,11 @@ function HttpRequest(sUrl,fpCallback){
 	var request = new XMLHttpRequest();
 	request.open("Get",sUrl,true);
 
-	var tempRequest = request;
 	//the request_readystate function detects whether the request for data was successful by checking for the 200 status
 	function request_readystatechange(){
-		if(tempRequest.readyState === 4 ){
-			if(tempRequest.status === 200){
-				fpCallback(tempRequest.responseText);
+		if(request.readyState === 4 ){
+			if(request.status === 200){
+				fpCallback(request.responseText);
 			}
 			//if the requst was not successful the error status code can be seen in the 
 			else{
