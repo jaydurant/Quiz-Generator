2dc2d8c54cc6c353a689142fc8703cb461342caf
durantjay <durantjay1@gmail.com>
2016-02-11 01:14:23 -0800

    Fix one for animations

--- js/main.js
+++ js/main.js
@@ -35,7 +35,7 @@ function changeQuestion(){
 		
 
 		//document.getElementById("quote-container").classList.remove("visible-hidden");
-		//document.getElementById("quote-container").classList.add("visible-shown");
+		document.getElementById("quote-container").classList.add("visible-shown");
 		document.getElementById("quote-text").innerHTML = quoteArray[randNumber]["quote-text"]; 
 		document.getElementById("quote-author").innerHTML = quoteArray[randNumber]["quote-author"];
 		currentQuote = randNumber;
