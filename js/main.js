f4df1bc19dfa214406cc5fc199c604a5ae1b3478
durantjay <durantjay1@gmail.com>
2016-02-11 00:32:21 -0800

    comment the new quotes

--- js/main.js
+++ js/main.js
@@ -36,8 +36,8 @@ function changeQuestion(){
 
 		document.getElementById("quote-container").classList.remove("visible-hidden");
 		document.getElementById("quote-container").classList.add("visible-shown");
-		document.getElementById("quote-text").innerHTML = quoteArray[randNumber]["quote-text"]; 
-		document.getElementById("quote-author").innerHTML = quoteArray[randNumber]["quote-author"];
+		//document.getElementById("quote-text").innerHTML = quoteArray[randNumber]["quote-text"]; 
+		//document.getElementById("quote-author").innerHTML = quoteArray[randNumber]["quote-author"];
 		currentQuote = randNumber;
 	},500);	
 
